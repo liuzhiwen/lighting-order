ab982dee4c7ea35c1c5bf7be3577b0d59146d730
524409188@qq.com <>
2018-02-23 15:42:47 +0800

    first commit

---
+++ lighting-order/order/order-core/order-core-portal/src/main/java/com/lighting/ordercoreportal/OrdercoreportalApplication.java
@@ -0,0 +1,12 @@
+package com.lighting.ordercoreportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdercoreportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdercoreportalApplication.class, args);
+	}
+}
